cb6f7c41dd3c471633352bf4392c6202e8131fcb
zhengluqian <2661837382@qq.com>
2024-06-27 15:34:40 +0800

    [Title]:自定义SPI 机制实现自定义序列化器 [Description]:自定义SPI 机制实现自定义序列化器

--- rpc/src/main/java/com/nami/rpc/serializer/SerializerFactory.java
+++ rpc/src/main/java/com/nami/rpc/serializer/SerializerFactory.java
@@ -13,20 +13,14 @@ import java.util.Map;
  */
 public class SerializerFactory {
 
-    /**
-     * 序列化映射（用于实现单例）
-     */
-    private static final Map<String, Serializer> KEY_SERIALIZER_MAP = new HashMap<String, Serializer>(){{
-            put(SerializerKeys.JDK, new JdkSerializer());
-            put(SerializerKeys.JSON, new JsonSerializer());
-            put(SerializerKeys.HESSIAN, new HessianSerializer());
-            put(SerializerKeys.KRYO, new KryoSerializer());
-        }};
+    static {
+        SpiLoader.load(Serializer.class);
+    }
 
     /**
      * 默认序列化器
      */
-    private static final Serializer DEFAULT_SERIALIZER = KEY_SERIALIZER_MAP.get("jdk");
+    private static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
 
     /**
      * 获取实例
@@ -35,6 +29,6 @@ public class SerializerFactory {
      * @return
      */
     public static Serializer getInstance(String key) {
-        return KEY_SERIALIZER_MAP.getOrDefault(key, DEFAULT_SERIALIZER);
+        return SpiLoader.getInstance(Serializer.class, key);
     }
 }
